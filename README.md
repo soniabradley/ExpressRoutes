fddae0a49e71fb51b824e16e9f6978f74d229e00
soniab <sonia.bradley524@gmail.com>
2018-04-24 22:16:44 -0500

    added reddit notes

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # ExpressRoutes
 
+* ORDER OF ROUTES MATTER.
+
 * What is [package.json](https://docs.nodejitsu.com/articles/getting-started/npm/what-is-the-file-package-json/).
 
-* [--save](https://docs.npmjs.com/getting-started/using-a-package.json),  adds the package to “dependencies” in the package.son file.
+* [--save](https://docs.npmjs.com/getting-started/using-a-package.json),  adds the package to “dependencies” in package.son file.
 
+* Introduce `*` route matcher.  Code will be triggered with any .get request aside from the ones that have been designated.  
 
 * [Postman](https://www.getpostman.com/) API Development Environment.
--- app.js
+++ app.js
@@ -4,22 +4,38 @@ var app = express();
 var PORT = 3000;
 
 // "/" => "Howdy partner!"
-app.get("/", function(req,res){
+app.get("/", function (req, res) {
     res.send("Howdy partner!");
 });
 
 // "/bye" => "Goodbye to you!"
-app.get("/bye", function(req,res){
+app.get("/bye", function (req, res) {
     res.send("Goodbye to you!");
 });
 
 // "/dog"=> "Meow!"
-app.get("/dog", function(req,res){
+app.get("/dog", function (req, res) {
     console.log("This will post only when localhost:3000/dog is prompted");
     res.send("Meow!");
 });
 
-app.listen(PORT, function(){
-    console.log("Server is listening");
+// request parameters, dynamic webpage
+app.get("/r/:subredditName", function (req, res) {
+    var subreddit = req.params.subredditName;
+    res.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT");
 });
 
+app.get("/r/:subredditName/comments/:id/:title/", function (req, res) {
+    console.log(req.params);
+    res.send("You are now using Subreddit with comments, id, title!");
+});
+
+
+// Order of routes matter, if this is placed at top, it will always default to it
+app.get("*", function (req, res) {
+    res.send("aaah you found me!");
+});
+
+app.listen(PORT, function () {
+    console.log("Server is listening");
+});
